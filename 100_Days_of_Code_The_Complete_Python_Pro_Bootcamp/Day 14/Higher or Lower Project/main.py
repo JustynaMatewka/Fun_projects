bf97d6fdf14fd74de8f3c89d06810716c5f1da1a
JustynaMatewka <96336210+JustynaMatewka@users.noreply.github.com>
2024-09-26 13:19:31 +0200

    Changes after viewing course solution

--- 100_Days_of_Code_The_Complete_Python_Pro_Bootcamp/Day 14/Higher or Lower Project/main.py
+++ 100_Days_of_Code_The_Complete_Python_Pro_Bootcamp/Day 14/Higher or Lower Project/main.py
@@ -1,29 +1,28 @@
 from art import logo, vs
 import game_data
 import random
+random.seed()
 
 
 def random_game_data():
-    random.seed()
+    """Returns random data from given database (dictionary)"""
     return random.choice(game_data.data)
 
 
 def check_user_guess(A, B):
+    """Takes compare_A and compare_B values and returns if user guessed which of them have more followers
+    (returns boolean and value A/B with more followers)"""
     user_guess = input("Who has more followers? Type 'A' or 'B': ").upper()
-    answer = 'A'
-    greater_dict = A
 
-    if A['follower_count'] < B['follower_count']:
-        answer = 'B'
-        greater_dict = B
-
-    if user_guess == answer:
-        return [True, greater_dict]
-
-    return [False, greater_dict]
+    if A['follower_count'] > B['follower_count']:
+        return [user_guess == 'A', A]
+    else:
+        return [user_guess == 'B', B]
 
 
 def game(compare_A, user_score):
+    """Takes random compare_A and 0 as beginner user score
+    and recurrently process game main functionalities until user give failure answer"""
     compare_B = random_game_data()
 
     print(logo)
