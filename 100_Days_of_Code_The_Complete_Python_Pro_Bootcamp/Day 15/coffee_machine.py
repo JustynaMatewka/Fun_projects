4827ab681b2ed87ddd50d9259474510b5605a35b
JustynaMatewka <96336210+JustynaMatewka@users.noreply.github.com>
2024-09-26 15:45:16 +0200

    Changes after viewing the course solution

--- 100_Days_of_Code_The_Complete_Python_Pro_Bootcamp/Day 15/coffee_machine.py
+++ 100_Days_of_Code_The_Complete_Python_Pro_Bootcamp/Day 15/coffee_machine.py
@@ -24,57 +24,52 @@ MENU = {
     }
 }
 
-COINS = {
-    "quarters": 0.25,
-    "dimes": 0.10,
-    "nickles": 0.05,
-    "pennies": 0.01
-}
 
 def share_report(current_resources):
+    """Print formated resources report"""
     print(f"Water: {current_resources['water']}ml\n"
           f"Milk: {current_resources['milk']}ml\n"
           f"Coffee: {current_resources['coffee']}g\n"
           f"Money: ${current_resources['money']}")
 
+
 def check_resources(current_resources, coffee_type):
+    """Take machine resources and user coffee type and check if there are enough ingredients return True/False"""
     for ingredient in MENU[coffee_type]['ingredients']:
         if not MENU[coffee_type]['ingredients'][ingredient] <= current_resources[ingredient]:
             print(f"Sorry there is not enough {ingredient}.”")
             return False
     return True
 
+
 def collect_coins():
-    user_coins = {
-        "quarters": 0,
-        "dimes": 0,
-        "nickles": 0,
-        "pennies": 0
-    }
-
+    """Ask user to insert coins by its type and returns their calculated value"""
     print("Please insert coins.")
-    user_coins['quarters'] = int(input("How many quarters?: "))
-    user_coins['dimes'] = int(input("How many dimes?: "))
-    user_coins['nickles'] = int(input("How many nickles?: "))
-    user_coins['pennies'] = int(input("How many pennies?: "))
-    return calculate_coins(user_coins)
+    total = int(input("How many quarters?: ")) * 0.25
+    total += int(input("How many dimes?: ")) * 0.1
+    total += int(input("How many nickles?: ")) * 0.05
+    total += int(input("How many pennies?: ")) * 0.01
+    return total
 
-def calculate_coins(given_coins):
-    return COINS['quarters'] * given_coins['quarters'] + COINS['dimes'] * given_coins['dimes'] + COINS['nickles'] * given_coins['nickles'] + COINS['pennies'] * given_coins['pennies']
 
 def check_transaction_successful(current_resources, given_coins, coffee_type):
+    """Return True (and change to user if needed) when the payment is accepted,
+    or False if money is insufficient"""
     if given_coins < MENU[coffee_type]['cost']:
         print("Sorry that's not enough money. Money refunded.")
         return False
 
     if given_coins > MENU[coffee_type]['cost']:
-        change = given_coins - MENU[coffee_type]['cost']
+        change = round(given_coins - MENU[coffee_type]['cost'], 2)
         print(f"Here is ${change} dollars in change.")
 
     current_resources['money'] += MENU[coffee_type]['cost']
     return True
 
+
 def make_coffee(current_resources, coffee_type):
+    """Main part in this file
+    take machine resources and coffee type chosen by user and process coffee or return None if error occurs"""
     if check_resources(current_resources, coffee_type):
         if check_transaction_successful(current_resources, collect_coins(), coffee_type):
             for ingredient in MENU[coffee_type]['ingredients']:
@@ -89,6 +84,7 @@ def make_coffee(current_resources, coffee_type):
 
 
 turn_on = True
+#   Beginner values of machine resources
 resources = {
     "water": 10000,
     "milk": 5000,
