f53ea787e08bb577ecaa2c35b84617fd0620b767
JustynaMatewka <tysiulka1007@gmail.com>
2025-01-06 17:42:40 +0100

    Project Snake Game part 1

---
+++ 100_Days_of_Code_The_Complete_Python_Pro_Bootcamp/Day 20/main.py
@@ -0,0 +1,24 @@
+import time
+from turtle import Screen
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)    #helpful with smoothly moving snake
+snake = Snake()     #call function snake.Snake.create_snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.15)
+    snake.move()
+
+screen.exitonclick()
