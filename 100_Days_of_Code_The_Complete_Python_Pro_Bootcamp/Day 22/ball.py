54a4a7da8ea0b45f7a4e77bead75ad6260bc898e
JustynaMatewka <tysiulka1007@gmail.com>
2025-01-12 17:48:29 +0100

    Project Pong game

---
+++ 100_Days_of_Code_The_Complete_Python_Pro_Bootcamp/Day 22/ball.py
@@ -0,0 +1,29 @@
+from select import select
+from turtle import Turtle
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("pink")
+        self.shape("circle")
+        self.penup()
+        self.new_x = 10
+        self.new_y = 10
+        self.move_speed = 0.1
+
+    def move(self):
+        new_x = self.xcor() + self.new_x
+        new_y = self.ycor() + self.new_y
+        self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.new_y *= -1
+
+    def bounce_x(self):
+        self.new_x *= -1
+        self.move_speed += 0.5
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
